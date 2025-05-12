92ed997709d97f2e84aaac714a01f43eadd33a5c
SarathValluru <sarath@hyrgpt.com>
2025-05-12 13:24:19 +0530

    password_generator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,212 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+import React, { useState } from 'react';
+import {
+  Clipboard,
+  StyleSheet,
+  Switch,
+  Text, TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    container: {
+        margin: 20,
+        marginTop: 100,
+        padding: 20,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        borderWidth: 1,
+        shadowColor: 'grey',
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        elevation: 5,
+        backgroundColor: '#fff',
+    },
+    header: {
+        color: 'green',
+        textAlign: 'center',
+        fontSize: 30,
+        marginBottom: 10,
+    },
+    subHeader: {
+        textAlign: 'center',
+        fontSize: 18,
+        marginBottom: 10,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    label: {
+        flex: 1,
+        fontSize: 18,
+    },
+    input: {
+        flex: 2,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        fontSize: 16,
+    },
+    checkboxLabel: {
+        fontSize: 20,
+    },
+    button: {
+        padding: 13,
+        backgroundColor: '#007bff',
+        color: '#fff',
+        borderRadius: 5,
+        overflow: 'hidden',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+        margin: 15,
+
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    copyButton: {
+        marginLeft: 10,
+        backgroundColor: '#007bff',
+        color: '#fff',
+        borderRadius: 5,
+        overflow: 'hidden',
+        padding: 10,
+        fontSize: 14,
+    },
+    successMessage: {
+        color: 'green',
+        textAlign: 'center',
+        fontSize: 20,
+    },
+    checkbox: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginBottom: 10,
+},
+
 });
+
+const App = () => {
+
+
+    const [password, setPassword] = useState('');
+    const [passwordLength, setPasswordLength] = useState('12');
+    const [useSymbols, setUseSymbols] = useState(true);
+    const [useNumbers, setUseNumbers] = useState(true);
+    const [useLowerCase, setUseLowerCase] = useState(true);
+    const [useUpperCase, setUseUpperCase] = useState(true);
+    const [successMessage, setSuccessMessage] = useState('');
+
+
+    const generatePassword = () => {
+        let charset = '';
+        let newPassword = '';
+
+        if (useSymbols) charset += '!@#$%^&*()';
+        if (useNumbers) charset += '0123456789';
+        if (useLowerCase) charset += 'abcdefghijklmnopqrstuvwxyz';
+        if (useUpperCase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        for (let i = 0; i < parseInt(passwordLength); i++) {
+            newPassword +=
+                charset.charAt(Math.floor(
+                    Math.random() * charset.length));
+        }
+
+        setPassword(newPassword);
+    };
+ 
+    const copyToClipboard = () => {
+        Clipboard.setString(password);
+        setSuccessMessage('Password copied to clipboard!');
+        setTimeout(() => setSuccessMessage(''), 2000);
+    };
+
+    return (
+        <View style={styles.container}>
+           
+            <Text style={styles.subHeader}>
+                Random Password Generator
+            </Text>
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>
+                    Password Length:
+                </Text>
+                <TextInput
+                    keyboardType="numeric"
+                    value={passwordLength}
+                    onChangeText={(text) => setPasswordLength(text)}
+                    style={styles.input}
+                />
+            </View>
+            <View style={styles.checkbox}>
+                <Switch
+                    value={useSymbols}
+                    onValueChange={() => setUseSymbols(!useSymbols)}
+                />
+                <Text style={styles.checkboxLabel}>
+                    Symbols
+                </Text>
+            </View>
+            <View style={styles.checkbox}>
+                <Switch
+                    value={useNumbers}
+                    onValueChange={() => setUseNumbers(!useNumbers)}
+                />
+                <Text style={styles.checkboxLabel}>
+                    Numbers
+                </Text>
+            </View>
+            <View style={styles.checkbox}>
+                <Switch
+                    value={useLowerCase}
+                    onValueChange={() => setUseLowerCase(!useLowerCase)}
+                />
+                <Text style={styles.checkboxLabel}>
+                    LowerCase
+                </Text>
+            </View>
+            <View style={styles.checkbox}>
+                <Switch
+                    value={useUpperCase}
+                    onValueChange={() => setUseUpperCase(!useUpperCase)}
+                />
+                <Text style={styles.checkboxLabel}>UpperCase</Text>
+            </View>
+            <TouchableOpacity style={styles.button}
+                onPress={generatePassword}>
+                <Text style={styles.buttonText}>
+                    Generate Password
+                </Text>
+            </TouchableOpacity>
+            {password !== '' && (
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>
+                        Generated Password:
+                    </Text>
+                    <TextInput value={password}
+                        style={styles.input} />
+                    <TouchableOpacity style={styles.copyButton}
+                        onPress={copyToClipboard}>
+                        <Text style={styles.buttonText}>
+                            Copy
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+            {successMessage !== '' && 
+            <Text style={styles.successMessage}>
+                {successMessage}
+            </Text>}
+        </View>
+    );
+};
+
+export default App;
